212b6a5dab7ee70cc0e840f4e50240e2bbc1f036
benzkittisak <77055086+benzkittisak@users.noreply.github.com>
2021-04-30 16:50:21 +0700

    Add files via upload

---
+++ s/Component 2 – 1 (master).vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="component-2-–-1">
+        <content />
+        <sidebar />
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.component-2-–-1 {
+
+}
+</style>
